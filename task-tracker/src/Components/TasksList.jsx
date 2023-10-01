89cfec9e56f98426c6602b2e3f46affe957fdfd7
MunnaGupta17 <mgabd17@gmail.com>
2023-10-01 17:11:04 +0530

    set up a react app and created header adn task list components

---
+++ task-tracker/src/Components/TasksList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TasksList.css';
+
+const TasksList = () => {
+    return (
+        <div>
+            <h1>Tasks List</h1>
+        </div>
+    )
+}
+
+export default TasksList;
